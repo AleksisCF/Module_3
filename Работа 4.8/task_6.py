7bc560e7b61e85c6782099e21ee11ca028306e7a
AleksisCF <Aleksiscf1337@gmail.com>
2026-07-29 00:58:07 +0700

    Update task_6.py

--- Работа 4.8/task_6.py
+++ Работа 4.8/task_6.py
@@ -1,9 +1,8 @@
 Number_1 = int(input("Кубик Кости: "))
 Number_2 = int(input("Кубик владельца: "))
-print("Сумма:", Number_1 + Number_2)
-if Number_1 <= Number_2:
-    Number_1 - Number_2
+if Number_1 >= Number_2:
+    print(Number_1 - Number_2)
     print("Игрок платит")
 else:
+    print("Сумма:", Number_1 + Number_2)
     print("Владелец платит")
-print("Игра окончена")
