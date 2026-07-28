d962515a39c53462791664c5d0035cb3fff141c7
AleksisCF <Aleksiscf1337@gmail.com>
2026-07-29 00:50:31 +0700

    Update task_3.py

--- Работа 4.8/task_3.py
+++ Работа 4.8/task_3.py
@@ -1,5 +1,5 @@
-Day_number = int(input("Какой сейчас день по счёту: "))
-if Day_number % 2:
-    print("На работу!")
+day_number = int(input("Какой сейчас день по счёту: "))
+if day_number % 2 == 0:
+    print("Идём на работу!")
 else:
-    print("Выходной!")
+    print("Сегодня выходной!")
