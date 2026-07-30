2fac7a60b6a8d29989e9778c1c117035bb23e3d0
AleksisCF <Aleksiscf1337@gmail.com>
2026-07-30 15:04:03 +0700

    Update task_1.py

--- Работа 5.6/task_1.py
+++ Работа 5.6/task_1.py
@@ -1,12 +1,12 @@
 experience_points = int(input("Введите количество очков опыта: "))
 
-level = 1
-
 if experience_points >= 5000:
     level = 4
 elif experience_points >= 2500:
     level = 3
-else:
+elif experience_points >= 1000:
     level = 2
+else:
+    level = 1
 
-print("Уровень персонажа:", level)
+print("Ваш уровень:", level)
