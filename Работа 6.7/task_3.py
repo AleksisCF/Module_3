8723ab6a5be07934a83204b3152e7da80d992997
AleksisCF <Aleksiscf1337@gmail.com>
2026-07-31 16:09:31 +0700

    Update task_3.py

--- Работа 6.7/task_3.py
+++ Работа 6.7/task_3.py
@@ -4,3 +4,14 @@ while number > 0:
     number = number // 10
     digit_count = digit_count + 1
 print("Количество цифр равно: ", digit_count)
+number = int(input("Введите число: "))
+if number == 0:
+    digit_count = 1
+else:
+    digit_count = 0
+    if number < 0:
+        number = -number
+    while number > 0:
+        number = number // 10
+        digit_count = digit_count + 1
+print("Количество цифр равно:", digit_count)
